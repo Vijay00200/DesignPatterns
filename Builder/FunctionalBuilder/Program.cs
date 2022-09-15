9cb6f7e336a8a1a649006941c1d13e4437a75f2a
vijay00200 <vijay.kumar00200@gmail.com>
2022-09-15 18:06:11 +0530

    functional builder

---
+++ Builder/FunctionalBuilder/Program.cs
@@ -0,0 +1,53 @@
+﻿//functional builder - adhereto open close , we will extend the builder class rathert then modify it
+
+using System;
+using System.Collections.Generic;
+
+public class Person
+{
+    public string Name, Position;
+    public override string ToString()
+    {
+        return $"Name :{Name}, Position: {Position}";
+    }
+}
+
+public abstract class FunctionalBuilder<TSubject, TSelf>
+where TSelf : FunctionalBuilder<TSubject, TSelf>
+where TSubject : new()
+{
+    private readonly List<Func<TSubject, TSubject>> actions = new List<Func<TSubject, TSubject>>();
+
+    public TSelf Do(Action<TSubject> action) => AddAction(action);
+    public TSubject Build() => actions.Aggregate(new TSubject(), (p, f) => f(p));
+
+    private TSelf AddAction(Action<TSubject> action)
+    {
+        actions.Add(p => { action(p); return p; });
+        return (TSelf)this;
+    }
+}
+
+public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>
+{
+    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+}
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorkAs(this PersonBuilder builder, string position)
+    => builder.Do(p => p.Position = position);
+}
+
+public class Demo
+{
+    static void Main(string[] args)
+    {
+        var Person = new PersonBuilder()
+                        .Called("Vijay")
+                        .WorkAs("Developer")
+                        .Build();
+
+        Console.WriteLine(Person.ToString());
+    }
+}
