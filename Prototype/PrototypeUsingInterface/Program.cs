856d7d1d4cda8d6176f576a4d933af0140e2abe8
vijay00200 <vijay.kumar00200@gmail.com>
2022-09-25 20:57:49 +0530

    prototype design pattern

---
+++ Prototype/PrototypeUsingInterface/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+    public class Point : IPrototype<Point>
+    {
+        public int X, Y;
+
+        public Point DeepCopy()
+        {
+            return new Point { X = this.X, Y = this.Y };
+        }
+    }
+
+    public class Line : IPrototype<Line>
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            return new Line { Start = this.Start.DeepCopy(), End = this.End.DeepCopy() };
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Line { Start = new Point { X = 0, Y = 1 }, End = new Point { X = 0, Y = 2 } };
+            var p2 = p1.DeepCopy();
+
+
+        }
+    }
+}
