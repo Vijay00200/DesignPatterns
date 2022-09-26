6b81c19cc89c5fd4d6ebe0486fc179e41638be6f
vijay00200 <vijay.kumar00200@gmail.com>
2022-09-26 12:12:49 +0530

    singleton design pattern

---
+++ Singleton/singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+interface IDatabase
+{
+    int GetPopulation(string name);
+}
+
+class SingletonDatabase : IDatabase
+{
+    private Dictionary<string, int> capitals;
+    private SingletonDatabase()
+    {
+        System.Console.WriteLine("Intializing database");
+
+        capitals = File.ReadAllLines("capital.txt").ToDictionary(x => x.Split(':')[0], x => int.Parse(x.Split(':')[1]));
+    }
+
+    public int GetPopulation(string name)
+    {
+        return capitals[name];
+    }
+
+    private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+
+    public static SingletonDatabase Instance => instance.Value;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var db = SingletonDatabase.Instance;
+        var city = "bbsr";
+        System.Console.WriteLine($"city {city} has a population of {db.GetPopulation(city)}");
+
+    }
+}
