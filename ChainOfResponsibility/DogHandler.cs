602baefe81dd377767a4dc0a3e4876f9a7f72322
Vijay kumar <vijay00200@live.com>
2024-06-09 16:28:43 +0530

    chain of responsibility

---
+++ ChainOfResponsibility/DogHandler.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ChainOfResponsibility
+{
+    class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+            {
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+}
---
+++ ChainOfResponsibility/SquirrelHandler.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ChainOfResponsibility
+{
+ class SquirrelHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "Nut")
+            {
+                return $"Squirrel: I'll eat the {request.ToString()}.\n";
+            }
+                return base.Handle(request);
+        }
+    }
+}
